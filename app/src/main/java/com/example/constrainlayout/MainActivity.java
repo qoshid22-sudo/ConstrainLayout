2247115e005d2807c9ab5073f12ad413e2d23079
rsydnur <qoshid22@gmail.com>
2026-04-15 14:14:40 +0700

    commit

--- app/src/main/java/com/example/constrainlayout/MainActivity.java
+++ app/src/main/java/com/example/constrainlayout/MainActivity.java
@@ -15,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        // Cari ConstraintLayout utama (root layout) - tidak perlu ID karena bisa langsung dari content view
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
